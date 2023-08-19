1853bb7eacb9d27e380c3a5d83e4e1e95ba52a00
bnuri00 <bnurik104@gmail.com>
2023-08-20 01:18:08 +0900

    feat: 공통응답 record 이름 수정, GlobalExceptionHandler 수정, ErrorCode 추가 및 수정
    - ResponseDto -> ApiResponse로 변경
    - GlobalExceptionHandler에 HttpRequestMethodNotSupportedException, RuntimeException 핸들링 추가

--- pointapp/src/main/java/com/workjo/pointapp/config/exception/GlobalExceptionHandler.java
+++ pointapp/src/main/java/com/workjo/pointapp/config/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.workjo.pointapp.config.exception;
 
-import com.workjo.pointapp.common.ResponseDto;
+
+import com.workjo.pointapp.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,11 +15,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class GlobalExceptionHandler {
 
-    /* Custom Exception */
-    @ExceptionHandler(CustomException.class)
-    protected ResponseEntity<ResponseDto<Void>> handleCustomException(final CustomException e) {
-        log.error("handleCustomException: {}", e.getErrorCode());
-        return new ResponseEntity<>(ResponseDto.ofError(e.getErrorCode()), HttpStatus.valueOf(e.getErrorCode().getStatus()));
-    }
+	/* Custom Exception */
+	@ExceptionHandler(CustomException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleCustomException(final CustomException e) {
+		log.error("handleCustomException: {}", e.getErrorCode());
+		return new ResponseEntity<>(ApiResponse.ofError(e.getErrorCode()), HttpStatus.valueOf(e.getErrorCode().getStatus()));
+	}
+
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleHttpRequestMethodNotSupportedException() {
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.METHOD_NOT_ALLOWED), HttpStatusCode.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value()));
+	}
+
+
+	@ExceptionHandler(RuntimeException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleRuntimeException(final RuntimeException e) {
+		log.error(e.getMessage(), e);
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.INTERNAL_SERVER_ERROR), HttpStatusCode.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+	}
 
 }
