08704191fa1584820bc1e1da77be8deb85982934
Soeng-dev <tjddyd1565@naver.com>
2023-08-24 23:17:35 +0900

    불필요한 메서드 삭제

--- pointapp/src/main/java/com/workjo/pointapp/point/infrastructure/IPointEarnRepository.java
+++ pointapp/src/main/java/com/workjo/pointapp/point/infrastructure/IPointEarnRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IPointEarnRepository extends JpaRepository<PointEarn, Long> {
-//    PointEarn savePointEarn(PointEarn pointEarn);
 }
