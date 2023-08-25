58e4217b23f8cf83692aa38b78f09c768b56db40
bnuri00 <bnurik104@gmail.com>
2023-08-25 15:14:27 +0900

    cleanup: vo에 JsonNaming 추가 - json 직렬화, 역직렬화시 snake case 사용 - 사용하지 않는 vo 삭제

--- pointapp/src/main/java/com/workjo/pointapp/user/vo/UserSignUpIn.java
+++ pointapp/src/main/java/com/workjo/pointapp/user/vo/UserSignUpIn.java
@@ -1,12 +1,15 @@
 package com.workjo.pointapp.user.vo;
 
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.ToString;
 
 
 @Getter
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserSignUpIn {
 
 	private String loginId;
