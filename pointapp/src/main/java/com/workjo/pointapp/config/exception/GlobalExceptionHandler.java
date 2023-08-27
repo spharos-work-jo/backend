360e3ff0e94acaa35045dc4bceb6d765d0355ec5
bnuri00 <bnurik104@gmail.com>
2023-08-27 17:55:33 +0900

    fix: 에러 핸들링 추가 (BadCredentialsException, MethodArgumentNotValidException) 및 시큐리티 필터 중 토큰 오류 핸들링 간단하게만 추가 - JwtAuthenticationFilter 및 token provider 리팩토링 필요

--- pointapp/src/main/java/com/workjo/pointapp/config/exception/GlobalExceptionHandler.java
+++ pointapp/src/main/java/com/workjo/pointapp/config/exception/GlobalExceptionHandler.java
@@ -3,10 +3,10 @@ package com.workjo.pointapp.config.exception;
 
 import com.workjo.pointapp.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -19,20 +19,32 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(CustomException.class)
 	protected ResponseEntity<ApiResponse<Void>> handleCustomException(final CustomException e) {
 		log.error("handleCustomException: {}", e.getErrorCode());
-		return new ResponseEntity<>(ApiResponse.ofError(e.getErrorCode()), HttpStatus.valueOf(e.getErrorCode().getStatus()));
+		return new ResponseEntity<>(ApiResponse.ofError(e.getErrorCode()), e.getErrorCode().getStatus());
 	}
 
 
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	protected ResponseEntity<ApiResponse<Void>> handleHttpRequestMethodNotSupportedException() {
-		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.METHOD_NOT_ALLOWED), HttpStatusCode.valueOf(HttpStatus.METHOD_NOT_ALLOWED.value()));
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.METHOD_NOT_ALLOWED), ErrorCode.METHOD_NOT_ALLOWED.getStatus());
+	}
+
+
+	@ExceptionHandler(BadCredentialsException.class)
+	protected ResponseEntity<ApiResponse<Void>> handleBadCredentialsException() {
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.FAIL_LOGIN), ErrorCode.FAIL_LOGIN.getStatus());
+	}
+
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ApiResponse<Void>> methodArgumentValidException() {
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.BAD_REQUEST), ErrorCode.BAD_REQUEST.getStatus());
 	}
 
 
 	@ExceptionHandler(RuntimeException.class)
 	protected ResponseEntity<ApiResponse<Void>> handleRuntimeException(final RuntimeException e) {
 		log.error(e.getMessage(), e);
-		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.INTERNAL_SERVER_ERROR), HttpStatusCode.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()));
+		return new ResponseEntity<>(ApiResponse.ofError(ErrorCode.INTERNAL_SERVER_ERROR), ErrorCode.INTERNAL_SERVER_ERROR.getStatus());
 	}
 
 }
