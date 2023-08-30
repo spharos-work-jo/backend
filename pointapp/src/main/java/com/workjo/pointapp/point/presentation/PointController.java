26e0db38fba94f7da4c5f15e3156f8ac5557f554
김성용 <tjddyd1565@naver.com>
2023-08-30 14:50:18 +0900

    refactor: 포인트 선물 작업 중, authentication 객체 리팩토링 전 merge 위한 commit

--- pointapp/src/main/java/com/workjo/pointapp/point/presentation/PointController.java
+++ pointapp/src/main/java/com/workjo/pointapp/point/presentation/PointController.java
@@ -4,11 +4,11 @@ import com.workjo.pointapp.auth.AuthService;
 import com.workjo.pointapp.common.ApiResponse;
 import com.workjo.pointapp.point.application.pointpolicy.IPointPolicy;
 import com.workjo.pointapp.point.application.IPointService;
-import com.workjo.pointapp.point.dto.PointDto;
-import com.workjo.pointapp.point.dto.PointEarnDto;
-import com.workjo.pointapp.point.dto.PointHistoryDto;
-import com.workjo.pointapp.point.vo.request.PointGiftReq;
-import com.workjo.pointapp.point.vo.response.PointGiftRes;
+import com.workjo.pointapp.point.domain.PointGiftStatus;
+import com.workjo.pointapp.point.dto.*;
+import com.workjo.pointapp.point.vo.request.PointGiftGiveReq;
+import com.workjo.pointapp.point.vo.request.PointGiftReplyReq;
+import com.workjo.pointapp.point.vo.response.PointGiftGiveRes;
 import com.workjo.pointapp.point.vo.request.PointEarnReq;
 import com.workjo.pointapp.point.vo.request.PointHistoryReq;
 import com.workjo.pointapp.point.vo.response.PointEarnRes;
@@ -35,22 +35,48 @@ public class PointController {
     private final ModelMapper modelMapper;
     private final AuthService authService;
 
+//todo 포인트 선물 전체 조회는 서비스단에 pointHistory 메서드로 기능 구현하기
+//todo 미수락 받은 포인트 조회는 포인트 적립 테이블에 서칭하도록 따로 구현
 
-//    @PostMapping("/gift")
-//    public ApiResponse<PointGiftRes>(
-//    @RequestBody
-//    PointGiftReq request)
-//
-//    {
-////todo 포인트 선물 시 이름/전화번호로 사용자의 이름/로그인 id 조회, 보내려는 사용자 맞는지 확인 후 선물할 포인트 량과 포인트 비밀번호 입력해서 보냄, 이를 고려해서 api 상세하게 수정 후 구현할것
-//    }
+    @PostMapping("/gift/give")
+    public ApiResponse<PointGiftGiveRes> givePointGift
+            (
+                    @RequestBody PointGiftGiveReq request,
+                    Authentication auth
+            ) {
+        //todo 포인트 패스워드 고려해 리팩토링
+        PointGiftCreateDto dto = new PointGiftCreateDto();
+        modelMapper.map(request, dto);
+        dto.setFromUserUuid(authService.getCurrentUserUUID(auth));
+        dto.setToUserUuid(UUID.fromString(request.getToUserUuid()));
+        pointService.givePointGift(dto);
+
+        return ApiResponse.ofSuccess(null);
+    }
+
+
+    @PatchMapping("/gift/{point_gift_id}/{gift_reply}")
+    public ApiResponse replyPointGift
+            (
+                    @PathVariable Long pointGiftId,
+                    @PathVariable String giftReply,
+                    Authentication auth,
+                    @RequestBody PointGiftReplyReq request
+            ) {
+        PointGiftReplyDto replyDto = new PointGiftReplyDto();
+        modelMapper.map(request, replyDto);
+
+
+
+        pointService.replyPointGift(replyDto);
+
+        return ApiResponse.ofSuccess(null);
+    }
 
 
     @PostMapping("/new")
     public ApiResponse<PointEarnRes> earnPoint
-            (
-                    @RequestBody PointEarnReq request
-            ) {
+            (@RequestBody PointEarnReq request) {
         //todo bill db 연결하기
         //find bill from db,
         // check if already saved point from bill
