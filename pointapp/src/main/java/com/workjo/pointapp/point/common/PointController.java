adc0028977c78d5942a1d959ce88464d362ab68b
bnuri00 <bnurik104@gmail.com>
2023-09-12 15:25:54 +0900

    feat: 포인트 정보 조회 (simple-info) API 구현

--- pointapp/src/main/java/com/workjo/pointapp/point/common/PointController.java
+++ pointapp/src/main/java/com/workjo/pointapp/point/common/PointController.java
@@ -1,12 +1,14 @@
 package com.workjo.pointapp.point.common;
 
+
 import com.workjo.pointapp.auth.AuthService;
 import com.workjo.pointapp.auth.AuthUtils;
 import com.workjo.pointapp.common.ApiResponse;
 import com.workjo.pointapp.point.common.application.IPointService;
-import com.workjo.pointapp.point.common.vo.response.PointEntityRes;
 import com.workjo.pointapp.point.common.domain.PointType;
 import com.workjo.pointapp.point.common.dto.CreatePointDto;
+import com.workjo.pointapp.point.common.vo.response.PointEntityRes;
+import com.workjo.pointapp.point.common.vo.response.PointSimpleInfoRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -20,25 +22,35 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/point")
 public class PointController {
 
-    private final IPointService pointService;
-    private final ModelMapper modelMapper;
-    private final AuthService authService;
+	private final IPointService pointService;
+	private final ModelMapper modelMapper;
+	private final AuthService authService;
 
-//todo 포인트 선물 전체 조회는 서비스단에 pointHistory 메서드로 기능 구현하기
 
-    @PostMapping("/addtest/{point}")//todo 테스트용 코드
-    public ApiResponse addPoint(Authentication auth, @PathVariable int point) {
-        CreatePointDto createDto = new CreatePointDto(
-                AuthUtils.getCurrentUserUUID(auth),
-                point,
-                PointType.ETC,
-                "테스트"
-        );
+	@PostMapping("/addtest/{point}")
+	public ApiResponse addPoint(Authentication auth, @PathVariable int point) {
+		CreatePointDto createDto = new CreatePointDto(
+			AuthUtils.getCurrentUserUUID(auth),
+			point,
+			PointType.ETC,
+			"테스트"
+		);
 
-        return ApiResponse.ofSuccess(modelMapper.map(
-                pointService.addPoint(createDto), PointEntityRes.class
-        ));
-    }
+		return ApiResponse.ofSuccess(modelMapper.map(
+			pointService.addPoint(createDto), PointEntityRes.class
+		));
+	}
 
+
+	@GetMapping("/simple-info")
+	public ApiResponse<PointSimpleInfoRes> getSimplePointInfo(Authentication authentication) {
+		Integer totalPoint = pointService.getTotalPoint(AuthUtils.getCurrentUserUUID(authentication));
+
+		return ApiResponse.ofSuccess(
+			PointSimpleInfoRes.builder()
+				.usableTotalPoint(totalPoint)
+				.build()
+		);
+	}
 
 }
