0ef26eb3aa2c3d6ef15276a3f38c951ab0c41a29
bnuri00 <bnurik104@gmail.com>
2023-09-12 15:37:49 +0900

    feat: 포인트 정보 조회 목업 (/point/info) - 내부 로직 추후 구현 필요 (정산처리 후 -> expPoint1, expPoint2 데이터 추가)

--- pointapp/src/main/java/com/workjo/pointapp/point/common/PointController.java
+++ pointapp/src/main/java/com/workjo/pointapp/point/common/PointController.java
@@ -8,12 +8,15 @@ import com.workjo.pointapp.point.common.application.IPointService;
 import com.workjo.pointapp.point.common.domain.PointType;
 import com.workjo.pointapp.point.common.dto.CreatePointDto;
 import com.workjo.pointapp.point.common.vo.response.PointEntityRes;
+import com.workjo.pointapp.point.common.vo.response.PointInfoRes;
 import com.workjo.pointapp.point.common.vo.response.PointSimpleInfoRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 
 @RestController
@@ -43,12 +46,28 @@ public class PointController {
 
 
 	@GetMapping("/simple-info")
-	public ApiResponse<PointSimpleInfoRes> getSimplePointInfo(Authentication authentication) {
+	public ApiResponse<PointSimpleInfoRes> getPointSimpleInfo(Authentication authentication) {
 		Integer totalPoint = pointService.getTotalPoint(AuthUtils.getCurrentUserUUID(authentication));
 
 		return ApiResponse.ofSuccess(
 			PointSimpleInfoRes.builder()
 				.usableTotalPoint(totalPoint)
+				.build()
+		);
+	}
+
+
+	@GetMapping("/info")
+	public ApiResponse<PointInfoRes> getPointInfo(Authentication authentication) {
+		Integer totalPoint = pointService.getTotalPoint(AuthUtils.getCurrentUserUUID(authentication));
+		LocalDate monthFirstDay = LocalDate.now().withDayOfMonth(1);
+		LocalDate expDate1 = monthFirstDay.plusMonths(1); // todo 내부 로직으로 변경
+		LocalDate expDate2 = monthFirstDay.plusMonths(2);
+		return ApiResponse.ofSuccess(
+			PointInfoRes.builder()
+				.usableTotalPoint(totalPoint)
+				.expDate1(expDate1)
+				.expDate2(expDate2)
 				.build()
 		);
 	}
