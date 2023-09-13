080735f8a81dbb69a25cab72c6d5318fabe23f71
bnuri00 <bnurik104@gmail.com>
2023-09-13 17:17:09 +0900

    feat: endDate가 어제까지인 쿠폰 만료처리 scheduler 구현

--- pointapp/src/main/java/com/workjo/pointapp/coupon/infrastructure/CouponRepository.java
+++ pointapp/src/main/java/com/workjo/pointapp/coupon/infrastructure/CouponRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
 	Slice<CouponIdDao> getByStartDateIsLessThanEqualAndEndDateIsGreaterThanEqual(LocalDate now1, LocalDate now2, Pageable pageable);
+	List<Coupon> findByEndDate(LocalDate time);
 
 }
